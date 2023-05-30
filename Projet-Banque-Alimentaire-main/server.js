707e28909a24162e21779415fb4e2bed164cf2f0
MoMo-Meb <2087660@bdeb.qc.ca>
2023-05-30 00:13:08 -0400

    aa

--- Projet-Banque-Alimentaire-main/server.js
+++ Projet-Banque-Alimentaire-main/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const sendEmail = require("./sendEmail");
